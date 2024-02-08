a19b39014eac5e7c970b7df0ce6c04d87b93c3f5
abdurohman-karim <ghostmagic766@gmail.com>
2024-02-08 15:54:50 +0500

    finished

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import getArgs from "./helpers/args.js";
 import { getWeather } from "./services/api.service.js";
-import { printError, printSuccess, printHelp } from "./services/log.service.js";
+import {
+  printError,
+  printSuccess,
+  printHelp,
+  printWeather,
+} from "./services/log.service.js";
 import { getKeyValue, saveKeyValue } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
@@ -17,32 +22,33 @@ const saveToken = async (token) => {
 };
 
 const saveCity = async (city) => {
-    if (!city.length) {
-      printError("City not provided!");
-      return;
-    }
-    try {
-      await saveKeyValue("city", city);
-      printSuccess("City was saved");
-    } catch (error) {
-      printError(error.message);
-    }
-  };
+  if (!city.length) {
+    printError("City not provided!");
+    return;
+  }
+  try {
+    await saveKeyValue("city", city);
+    printSuccess("City was saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const getForcast = async () => {
-    try{
-        const city = process.env.CITY ?? (await getKeyValue('city'))
-        const response = await getWeather(city)
-        console.log(response);
-    } catch (error) {
-        if(error?.response?.status == 404){
-            printError('City not found')
-        } else if(error?.response?.status == 401){
-            printError('Invalid token used')
-        } else{ 
-            printError(error.message)
-        }
+  try {
+    const city = process.env.CITY ?? (await getKeyValue("city"));
+    const response = await getWeather(city);
+    printWeather(response);
+    // console.log(response);
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError("City not found");
+    } else if (error?.response?.status == 401) {
+      printError("Invalid token used");
+    } else {
+      printError(error.message);
     }
+  }
 };
 const startCLI = () => {
   const args = getArgs(process.argv);
@@ -53,7 +59,7 @@ const startCLI = () => {
     return saveCity(args.s);
   } else if (args.t) {
     return saveToken(args.t);
-  } 
+  }
   return getForcast();
 };
 
--- services/log.service.js
+++ services/log.service.js
@@ -18,4 +18,19 @@ const printHelp = () => {
     `);
 };
 
-export { printError, printSuccess, printHelp };
+const printWeather = (response) => {
+    function kelvinToCelsius(kelvinTemp) {
+        return Math.round(kelvinTemp - 273.15)
+    }
+  console.log(dedent
+    `
+        ${chalk.cyan("WEATHER")}: ${response.name} / ${response.weather[0].description}
+        ${chalk.cyan("TEMPERATURE")}: ${kelvinToCelsius(response.main.temp)} °С (feels like ${kelvinToCelsius(response.main.feels_like)})
+        ${chalk.cyan("HUMIDITY")}: ${response.main.humidity} %
+        ${chalk.cyan("WIND")}: ${response.wind.speed} m/s
+
+    `
+  );
+};
+
+export { printError, printSuccess, printHelp, printWeather };
